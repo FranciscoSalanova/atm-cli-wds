a798768524c4f28f052d03af1254abe295533a70
Francisco Salanova <fs185092@ncr.com>
2024-03-16 18:53:46 -0300

    Adding create account, deposit and withdraw features

--- Account.js
+++ Account.js
@@ -1,4 +1,4 @@
-const FileSystem = require('./FileSystem.js')
+const FileSystem = require("./FileSystem.js")
 
 module.exports = class Account {
   constructor(name) {
@@ -32,5 +32,28 @@ module.exports = class Account {
 
   async #load() {
     this.#balance = parseFloat(await FileSystem.read(this.filePath))
+  }
+
+  static async create(accountName) {
+    const account = new Account(accountName)
+
+    await FileSystem.write(account.filePath, 0)
+    account.#balance = 0
+
+    return account
+  }
+
+  async deposit(amount) {
+    await FileSystem.write(this.filePath, this.#balance + amount)
+    this.#balance = this.#balance + amount
+  }
+
+  async withdraw(amount) {
+    if (amount <= this.#balance) {
+      await FileSystem.write(this.filePath, this.#balance - amount)
+      this.#balance = this.#balance - amount
+    } else {
+      console.log("Insufficient fonds.")
+    }
   }
 }
--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-// ask for account
-// if account doesn't exist, ask to create an account
-// ask what they want to do
 // execute command
 // view
 // withdraw
 // deposit
 
-const Account = require('./Account.js')
-const CommandLine = require('./CommandLine.js')
+const Account = require("./Account.js")
+const CommandLine = require("./CommandLine.js")
 
 async function main() {
   const accountName = await CommandLine.ask(
-    'Which account do you like to access?'
+    "Which account do you like to access?"
   )
 
   const account = await Account.find(accountName)
-  if (account) {
-    console.log('Found account')
-  } else {
-    console.error('Cannot find account')
+  if (account == null) account = await promptCreateAccount(accountName)
+  if (account != null) await promptTask(account)
+}
+
+async function promptCreateAccount(accountName) {
+  const response = await CommandLine.ask(
+    "That account does not exist, would you like to create it? (yes/no)"
+  )
+
+  if (response === "yes") {
+    return await Account.create(accountName)
+  }
+}
+
+async function promptTask(account) {
+  const response = await CommandLine.ask(
+    "What would you like to do? (view/deposit/withdraw)"
+  )
+
+  if (response === "deposit") {
+    const amount = parseFloat(await CommandLine.ask("How much?"))
+    await account.deposit(amount)
+    CommandLine.print(`Your current balance is $${account.balance}`)
+  }
+  if (response === "withdraw") {
+    const amount = parseFloat(await CommandLine.ask("How much?"))
+    await account.withdraw(amount)
+    CommandLine.print(`Your current balance is $${account.balance}`)
   }
 }
 
